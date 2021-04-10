ba0a011fc478c7a7a89277f9e233c19f9ee70165
Felipe F <felipe.fs.vr@gmail.com>
2021-04-09 21:14:55 -0300

    Init

---
+++ WebApi/Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApi.Models.Usuarios;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+      
+        [HttpPost]
+        public IActionResult Logar(LoginViewModelInput _loginViewModelInput){
+
+            return Created("", _loginViewModelInput);
+        }
+
+        
+        public IActionResult Registrar(RegistrarViewModelInput _registrarViewModelInput){
+
+            return Created("", _registrarViewModelInput);
+        }
+
+       
+    }
+}
---
+++ WebApi/Models/Usuarios/RegistrarViewModelInput.cs
@@ -0,0 +1,8 @@
+namespace WebApi.Models.Usuarios
+{
+    public class RegistrarViewModelInput
+    {
+        public string Login { get; set; }
+        public string  Senha { get; set; }
+    }
+}
